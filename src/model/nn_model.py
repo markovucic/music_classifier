de35703f9090c8e1b1bbf4e22b1d7d2e370d267d
marko vucic <marko03.vucic@gmail.com>
2026-09-05 22:47:10 +0200

    refactored nn conv models

--- src/model/nn_model.py
+++ src/model/nn_model.py
@@ -55,25 +55,6 @@ class SpectrogramDataset(Dataset):
         return x, int(self.y[idx])
 
 
-class SimpleCNN(nn.Module):
-    def __init__(self, n_classes):
-        super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 16, 3, padding=1), nn.BatchNorm2d(16), nn.ReLU(), nn.MaxPool2d(2),
-            nn.Conv2d(16, 32, 3, padding=1), nn.BatchNorm2d(32), nn.ReLU(), nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, 3, padding=1), nn.BatchNorm2d(64), nn.ReLU(), nn.MaxPool2d(2),
-        )
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.dropout = nn.Dropout(0.5)
-        self.fc = nn.Linear(64, n_classes)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.pool(x).flatten(1)
-        x = self.dropout(x)
-        return self.fc(x)
-
-
 class ConvGRUNet(nn.Module):
     """Configurable CNN, optionally with a GRU over the time axis before pooling."""
 
@@ -113,7 +94,7 @@ class ConvGRUNet(nn.Module):
 
 
 MODEL_REGISTRY = {
-    "cnn_small": (ConvGRUNet, {"conv_channels": (8, 16, 32), "dropout": 0.5}),
+    "cnn_small": (ConvGRUNet, {"conv_channels": (8, 16), "dropout": 0.5}),
     "crnn_small": (ConvGRUNet, {"conv_channels": (8, 16), "gru_hidden": 32, "dropout": 0.5}),
 }
 
